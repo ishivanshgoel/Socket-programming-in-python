b87a7c11e6db4206c1cad16a7869a7b6fa0643ad
shivanshgoel-12 <53016763+shivanshgoel-12@users.noreply.github.com>
2020-03-25 23:25:13 +0530

    Update myclient.py

--- myclient.py
+++ myclient.py
@@ -1,22 +1,10 @@
 import socket
 s1=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s1.connect((socket.gethostname(),1024))
-
-
-s2=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s2.connect((socket.gethostname(),1025))
-
-
-s3=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s3.connect((socket.gethostname(),1026))
+s1.connect((socket.gethostname(),2002))
+msg1=""
 while True:
-	msg1=s1.recv(1024)                 
-	print(msg1.decode("utf-8")) 
-
-	msg2=s2.recv(1024)                 
-	print(msg2.decode("utf-8")) 
-
-	msg3=s3.recv(1024)                 
-	print(msg3.decode("utf-8")) 
-
-
+	v=s1.recv(10)
+	if len(v)==0:
+		break
+	msg1=msg1+v.decode("utf-8")          
+print(msg1)
