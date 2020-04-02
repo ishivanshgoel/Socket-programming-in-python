94c10b7e559573ae2c897be5373e1e709f423d56
Shivansh Goel <53016763+shivanshgoel-12@users.noreply.github.com>
2020-04-03 01:20:11 +0530

    Update server.py

--- multi_client server/server.py
+++ multi_client server/server.py
@@ -1,32 +1,38 @@
 ##server side
 import socket
 import _thread
-import threading 
+import threading
 import select
 
-
-print_lock = threading.Lock()
-
+""" Connect to clients one by one as instructed by program"""
 
 s1=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s1.bind((socket.gethostname(),2002))
+s1.bind((socket.gethostname(),5000))
 s1.listen(5)
-s1.setblocking(0)
+print("Connect to Truck 1....")
+clt1,address1=s1.accept()
+print("Successfully Connected to Truck 1")
 
+print("Connect to Truck 2.....")
 s2=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s2.bind((socket.gethostname(),2003))
+s2.bind((socket.gethostname(),5001))
 s2.listen(5)
-s2.setblocking(0)
+clt2,address2=s2.accept()
+print("Successfully Connected to Truck 2")
 
+
+print("Connect to Truck 3")
 s3=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s3.bind((socket.gethostname(),2004))
+s3.bind((socket.gethostname(),5002))
 s3.listen(5)
-s3.setblocking(0)
+clt3,address3=s3.accept()
+print("Successfully Connected to Truck 3")
 
-n=int(input())
+print("Connection Successful with all three Trucks")
 
+n=int(input("Enter Number of locations:"))
+print(f"Enter {n} locations and time")
 loc_dur=[]
-ans=[]
 for i in range(n):
     loc_dur.append(input().split(" "))
 
@@ -35,7 +41,6 @@ time2=0
 time3=0
 task=0
 
-
 try:
     f = open("myfile.txt", "x")
 except:
@@ -43,28 +48,25 @@ except:
 
 while(task!=n):
     if(time1==0):
-        print("Connecting to Truck 1")
+        print(f"Location {loc_dur[0][0]} Assigned to Truck 1")
         time1=int(loc_dur[0][1])
-        clt1,address1=s1.accept()
-        clt1.send(bytes(str(loc_dur[0][0]+" Truck 1\n"),"ascii"))
+        clt1.sendall(bytes(str(loc_dur[0][0]+" Truck 1\n"),"ascii"))
         f.write(str(loc_dur[0][0]+" Truck 1\n"))
         loc_dur=loc_dur[1:]
         task+=1
 
     if(time2==0):
-        print("Connecting to Truck 2")
+        print(f"Location {loc_dur[0][0]} Assigned to Truck 2")
         time2=int(loc_dur[0][1])
-        clt2,address2=s2.accept()
-        clt2.send(bytes(str(loc_dur[0][0]+" Truck 2\n"),"ascii"))
+        clt2.sendall(bytes(str(loc_dur[0][0]+" Truck 2\n"),"ascii"))
         f.write(str(loc_dur[0][0]+" Truck 2\n"))
         loc_dur=loc_dur[1:]
         task+=1
-    
+
     if(time3==0):
-        print("Connecting to Truck 3")
+        print(f"Location {loc_dur[0][0]} Assigned to Truck 3")
         time3=int(loc_dur[0][1])
-        clt3,address3=s3.accept()
-        clt3.send(bytes(str(loc_dur[0][0]+" Truck 3\n"),"ascii"))
+        clt3.sendall(bytes(str(loc_dur[0][0]+" Truck 3\n"),"ascii"))
         f.write(str(loc_dur[0][0]+" Truck 3\n"))
         loc_dur=loc_dur[1:]
         task+=1
