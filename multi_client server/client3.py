0861287e217e3d38e1298165c99772a131679bc9
Shivansh Goel <53016763+shivanshgoel-12@users.noreply.github.com>
2020-04-03 16:12:31 +0530

    Update client3.py

--- multi_client server/client3.py
+++ multi_client server/client3.py
@@ -1,16 +1,12 @@
 import socket
+
 s3=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s3.connect((socket.gethostname(),5002))
-msg=""
+s3.connect((socket.gethostname(),2002))
 print("*****************TRUCK 3 ACTIVE*************")
 while True:
-    k=s3.recv(10).decode('ascii')
+    k=s3.recv(1024).decode('ascii')
     if(len(k)>0):
-        msg=msg+k
+        print(k)
     else:
         break
-
-print(msg)
 s3.close()
-    
-        
