ae2bcea00b974788d6a3c19767144f29000f5fa3
Shivansh Goel <53016763+shivanshgoel-12@users.noreply.github.com>
2020-04-03 16:12:08 +0530

    Update client2.py

--- multi_client server/client2.py
+++ multi_client server/client2.py
@@ -1,18 +1,15 @@
 import socket
+
 s2=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s2.connect((socket.gethostname(),5001))
-msg=""
+s2.connect((socket.gethostname(),2001))
 print("*****************TRUCK 2 ACTIVE*************")
 while True:
-    k=s2.recv(10).decode('ascii')
+    k=s2.recv(1024).decode('ascii')
     if(len(k)>0):
-        msg=msg+k
+        print(k)
     else:
         break
-
-print(msg)
 s2.close()
-    
         
         
         
